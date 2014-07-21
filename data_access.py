c175b712ca25cd3e71ab8522802e2e783bfd368e
ZYB_MAC <zhanyingbo@Zhans-MacBook-Air.local>
2014-07-21 09:06:55 +0800

    home change 1

--- data_access.py
+++ data_access.py
@@ -9,22 +9,18 @@ progress_key = ndb.Key('Progress', 'default_progress')
 meeting_key = ndb.Key('Meeting', 'default_meeting')
 attendance_key = ndb.Key('Attendance', 'default_attendance')
 
-class Greeting(ndb.Model):
-  author = ndb.UserProperty()
-  content = ndb.TextProperty()
-  date = ndb.DateTimeProperty(auto_now_add=True)
 
 
 class Access(ndb.Model):
-    member_id = ndb.IntegerProperty()
+    member_id = ndb.KeyProperty()
     password = ndb.StringProperty()
 
     #the following, 0  self access, 1  all access,
-    member_access = ndb.IntegerProperty() # default 0
+    member_access = ndb.IntegerProperty(required=True, default=0) # default 0
     project_access = ndb.IntegerProperty() # default 1
-    progress_access = ndb.IntegerProperty() # default 0
+    progress_access = ndb.IntegerProperty(required=True, default=0) # default 0
     meeting_access = ndb.IntegerProperty() # default 1
-    attendance_access = ndb.IntegerProperty() # default 0
+    attendance_access = ndb.IntegerProperty(required=True, default=0) # default 0
 
     @classmethod
     def verify_access(cls, member_id_pass, password_pass):
@@ -32,24 +28,47 @@ class Access(ndb.Model):
 
 
 class Member(ndb.Model):
-    member_id = ndb.IntegerProperty()
+    member_id = ndb.KeyProperty(required=True)
+    english_name = ndb.StringProperty(required=True)
+    chinese_name = ndb.StringProperty(required=True)
+    salutation = ndb.StringProperty(required=True)#, choices=['Mr','Ms','Mrs','Miss'])
+    nric = ndb.StringProperty(required=True)
+    nationality = ndb.StringProperty(required=True)
+    join_time = ndb.DateProperty()#required=True)
+    title = ndb.StringProperty(required=True, choices=['CC','CL','DTM'])
+    date_of_birth = ndb.DateProperty()#required=True)
+    contact = ndb.IntegerProperty(required=True)
+    address = ndb.StringProperty()
+    email = ndb.StringProperty(required=True)
+    company = ndb.StringProperty()
+    industry = ndb.StringProperty()
+    job_title = ndb.StringProperty()
+
+    @classmethod
+    def retrieval_member_detail(cls, member_id_pass):
+      return cls.query(Member.member_id == int(member_id_pass))
+
+    @classmethod
+    def add_update_member_detail(cls, member_id_pass, member):
+      member.put()
+      return cls.retrieval_member_detail(member_id_pass)
 
 
 class Project(ndb.Model):
-    project_id = ndb.IntegerProperty()
+    project_id = ndb.KeyProperty()
 
 
 class Meeting(ndb.Model):
-    meeting_id = ndb.IntegerProperty()
+    meeting_id = ndb.KeyProperty()
 
 class Attendance(ndb.Model):
-    meeting_id = ndb.IntegerProperty()
-    member_id = ndb.IntegerProperty()
+    meeting_id = ndb.KeyProperty()
+    member_id = ndb.KeyProperty()
     appear = ndb.IntegerProperty() # 0 no 1 yes, default 0
 
 class Progress(ndb.Model):
-    member_id = ndb.IntegerProperty()
-    project_id = ndb.IntegerProperty()
-    meeting_id = ndb.IntegerProperty()
+    member_id = ndb.KeyProperty()
+    project_id = ndb.KeyProperty()
+    meeting_id = ndb.KeyProperty()
 
 
--- guestbook.py
+++ guestbook.py
@@ -19,50 +19,102 @@
 
 import os
 import webapp2
+import time
+import logging
+
 from google.appengine.ext.webapp.template import render
 
-from data_access import access_key, Access
 
-ACCESS_DETAIL = None
+from data_access import access_key, member_key, Access, Member
 
-class HomePage(webapp2.RequestHandler):
+
+KEY = 'member_id'
+class BasePage(webapp2.RequestHandler):
+  def redirect_cookie(self, url):
+    key_detail = self.request.cookies.get(KEY, None)
+
+    if key_detail is None:
+      key_detail = self.request.get(KEY, None)
+
+    expires = time.strftime("%a, %d-%b-%Y %T GMT", time.gmtime(time.time() +  0.5 * 3600 ))#  half an hour from now))
+    if key_detail is not None:
+      self.response.headers.add_header(
+        'Set-Cookie',
+        'member_id=%s; expires=%s'
+          % ( self.request.get(KEY).encode(), expires))
+    
+    self.redirect(url)
+
+
+class HomePage(BasePage):
   def get(self):
-    tmpl = os.path.join(os.path.dirname(__file__), 'home.html')
-    self.response.out.write(render(tmpl,{}))
+    template = os.path.join(os.path.dirname(__file__), 'home.html')
+    self.response.out.write(render(template,{}))
 
 
-class Login(webapp2.RequestHandler):
+class Login(BasePage):
   def post(self):
     access = Access(parent=access_key)
-    access_detail = access.verify_access(self.request.get('member_id'), self.request.get('password'))
+    logging.error(self.request)
+    access_detail = access.verify_access(self.request.get(KEY), self.request.get('password'))
     if access_detail:
-        self.redirect('/member')
-        ACCESS_DETAIL = access_detail
+        self.redirect_cookie('/member')
     else:
         self.redirect('/')
 
-class MemberPage(webapp2.RequestHandler):
+class MemberPage(BasePage):
   def get(self):
-    tmpl = os.path.join(os.path.dirname(__file__), 'member.html')
-    self.response.out.write(render(tmpl,{}))
+    if self.request.cookies.get(KEY) is None:
+      self.redirect('/')
+    member = Member(parent=member_key)
+    member_detail = member.retrieval_member_detail(self.request.cookies.get(KEY))
+    template = os.path.join(os.path.dirname(__file__), 'member.html')
+    self.response.out.write(render(template,member_detail))
 
 
+#
+#class SignUp(webapp2.RequestHandler):
+#  def post(self):
+#    access = Access(parent=access_key)
+#    try:
+#        access.member_id = int(self.request.get('member_id'))
+#        access.password = self.request.get('password')
+#        access.project_access = 1
+#        access.member_access = 0
+#        access.progress_access = 0
+#        access.attendance_access = 1
+#        access.meeting_access = 1
+#        access.put()
+#    except:
+#        pass
+#    self.redirect('/')
 
-class SignUp(webapp2.RequestHandler):
+
+class DetailUpdate(BasePage):
   def post(self):
-    access = Access(parent=access_key)
-    try:
-        access.member_id = int(self.request.get('member_id'))
-        access.password = self.request.get('password')
-        access.project_access = 1
-        access.member_access = 0
-        access.progress_access = 0
-        access.attendance_access = 1
-        access.meeting_access = 1
-        access.put()
-    except:
-        pass
-    self.redirect('/')
+    if self.request.cookies.get('member_id') is None:
+      self.redirect('/')
+    member = Member(parent=member_key)
+    member_detail = Member(
+      member_id = self.request.get('member_id'),
+      english_name = self.request.get('engish_name'),
+      chinese_name = self.request.get('chinese_name'),
+      salutation = self.request.get('salutation'),
+      nric = self.request.get('nric'),
+      nationality = self.request.get('nationality'),
+      join_time = self.request.get('join_time'),
+      title = self.request.get('title'),
+      date_of_birth = self.request.get('date_of_birth'),
+      contact = self.request.get('contact'),
+      address = self.request.get('address'),
+      email = self.request.get('email'),
+      company = self.request.get('company'),
+      industry = self.request.get('industry'),
+      job_title = self.request.get('job_title')
+    )
+    member.add_update_member_detail(self.request.cookies.get('member_id'), member_detail)
+    self.redirect_cookie('/member')
+
 
 
 class Dummy(webapp2.RequestHandler):
@@ -75,6 +127,7 @@ app = webapp2.WSGIApplication([
   ('/', HomePage),
   ('/login', Login),
   ('/member', MemberPage),
-  ('/signup', SignUp),
+  ('/add_update_detail', DetailUpdate),
+  #('/signup', SignUp),
   ('/home', Dummy),
 ], debug=True)
