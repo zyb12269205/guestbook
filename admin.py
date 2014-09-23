55c8094cfb8dda158ca0267c24d9230679360c76
ZYB_MAC <zhanyingbo@Zhans-MacBook-Air.local>
2014-09-23 09:00:54 +0800

    project

--- admin.py
+++ admin.py
@@ -38,25 +38,30 @@ class AdminMeetingPageClass(BasePage):
 class AdminMeetingAddUpdateClass(BasePage):
     def post(self):
         if not self.is_admin_log_in(): self.redirect('/')
-        meeting_id = int(self.request.get(MEETING_ID,'-1'))
+        try:
+            meeting_id = int(self.request.get(MEETING_ID,'0'))
+        except:
+            self.redirect_cookie(ADMIN_MEETING_LIST_PAGE)
+            return  
         meeting_select = Meeting(id=int(meeting_id),parent=meeting_key)
         meeting_select.meeting_time = self.request.get('meeting_time')
         meeting_select.key_name = meeting_id
-        try:
-            meeting_select.meeting_id = meeting_id
-            meeting_select.put()
-        except:
-            self.redirect_cookie('/')
-            return
+        meeting_select.meeting_id = meeting_id
+        meeting_select.put()
+
         self.redirect_cookie(ADMIN_MEETING_LIST_PAGE)
 
 class AdminMeetingDeleteClass(BasePage):
-    def post(self):
+    def get(self):
         if not self.is_admin_log_in(): self.redirect('/')
-        meeting_id = self.request.get(MEETING_ID,-1)
-        meeting = Meeting(parent=meeting_key)
-        meeting_select = meeting.get_or_insert(meeting_id)
-        meeting_select.key.delete()
+        try:
+            meeting_id = int(self.request.get(MEETING_ID,0))
+            meeting = Meeting(parent=meeting_key)
+            meeting_select = meeting.get_or_insert(meeting_id)
+            meeting_select.key.delete()
+        except:
+            pass
+        self.redirect_cookie(ADMIN_MEETING_LIST_PAGE)
 
 
 
@@ -84,27 +89,30 @@ class AdminProjectPageClass(BasePage):
 class AdminProjectAddUpdateClass(BasePage):
     def post(self):
         if not self.is_admin_log_in(): self.redirect('/')
-        project_id = int(self.request.get(PROJECT_ID,'-1'))
+        try:
+            project_id = int(self.request.get(PROJECT_ID,'0'))
+        except:
+            self.redirect_cookie(ADMIN_PROJECT_LIST_PAGE)
+            return
         project_select = Project(id=project_id,parent=project_key)
         project_select.project_name = self.request.get('project_name')
         project_select.project_requirement = self.request.get('project_requirement')
-        project_select.key_name = project_id
-        try:
-            project_select.project_id = project_id
-            project_select.put()
-        except:
-            self.redirect_cookie('/')
-            return
+        project_select.project_id = project_id
+        project_select.put()
+
         self.redirect_cookie(ADMIN_PROJECT_LIST_PAGE)
 
 class AdminProjectDeleteClass(BasePage):
-    def post(self):
+    def get(self):
         if not self.is_admin_log_in(): self.redirect('/')
-        project_id = self.request.get(PROJECT_ID,-1)
-        project = Project(parent=project_key)
-        project_select = project.get_or_insert(project_id)
-        project_select.key.delete()
+        try:
+            project_id = int(self.request.get(PROJECT_ID,0))
+            project = Project(id=project_id,parent=project_key)
+            project.key.delete()
+        except:
+            pass
         self.redirect_cookie(ADMIN_PROJECT_LIST_PAGE)
+
 
 
 class AdminMemberListPageClass(BasePage):
